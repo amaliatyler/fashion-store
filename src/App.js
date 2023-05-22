25efe03f960c5fd72e0f7182a019507e6f0a0c9d
Tyler <amaliatyler@gmail.com>
2023-05-22 08:16:09 +0300

    adding changes

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
       <Promo />
       <Brands />
       <Arrivals />
-      {/* <Sale /> */}
+      <Sale />
       <Favorite />
       <Application />
       <Footer />
--- src/components/sale/Sale.jsx
+++ src/components/sale/Sale.jsx
@@ -1,6 +1,6 @@
 import './sale.css';
 
-import saleImg from './../../img/images/promo-img.jpg';
+import saleImg from './../../img/images/sale-img.png';
 import BtnLink from '../btn/BtnLink';
 
 function Sale() {
@@ -9,18 +9,21 @@ function Sale() {
         <div className="container">
             <div className="sale__content">
                 <div className="sale__img">
+                    <svg class="star">
+                        <use href="~/sprite.svg#star"></use>
+                    </svg>
                     <img src={saleImg} alt="Sale" />
                 </div>
                 <div className="sale__text">
                     <div className="sale__title">
                         <span className="highlight">
-                            <span>Payday</span> Sale Now
-                        </span>
-                        <p className="sale__descr">Spend minimal $100 get 30% off
-voucher code for your next purchase</p>
-                        <p className="sale__date">1 June - 10 June 2021</p>
-                        <p className="sale__terms">*Terms & Conditions apply</p>
+                            <span>Payday</span>
+                        </span>Sale Now
                     </div>
+                    <p className="sale__descr">Spend minimal $100 get 30% off
+voucher code for your next purchase</p>
+                    <p className="sale__date">1 June - 10 June 2021</p>
+                    <p className="sale__terms">*Terms & Conditions apply</p>
                     <div className="sale__btn-wrapper">
                         <BtnLink className="sale__btn" innerText="Shop now"/>
                     </div>
