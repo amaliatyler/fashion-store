0071d26c1aa60863624c2ca93304611a9582abe4
Амалия Тайлер <AATayler@1cbit.ru>
2023-05-26 09:39:32 +0300

    scss files imported to main file

--- src/index.js
+++ src/index.js
@@ -2,8 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 
-import './styles/reset.scss';
-import './styles/common.scss';
+import './styles/main.scss';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
